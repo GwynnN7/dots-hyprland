340f2ad19fe5e6eec00eb8780263bed2fc914afd
GwynbleiddN7 <mattcheru03@gmail.com>
2025-11-01 12:15:50 +0100

    Edit fish and cortana

--- dots/.config/fish/config.fish
+++ dots/.config/fish/config.fish
@@ -10,6 +10,18 @@ function fish_greeting
     fastfetch
 end
 
+function y
+	set tmp (mktemp -t "yazi-cwd.XXXXXX")
+	yazi $argv --cwd-file="$tmp"
+	if read -z cwd < "$tmp"; and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+		builtin cd -- "$cwd"
+	end
+	rm -f -- "$tmp"
+end
+
+set -gx EDITOR nvim
+fish_add_path --append --path ~/.local/bin/
+
 if status is-interactive # Commands to run in interactive sessions can go here
 
     # No greeting
