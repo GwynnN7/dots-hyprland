5bf7a0ac931661ee1f4cbb2da9cb6897704294fb
GwynbleiddN7 <mattcheru03@gmail.com>
2025-11-04 13:39:32 +0100

    Fix and Cortana

--- sdata/custom/scripts.sh
+++ sdata/custom/scripts.sh
@@ -2,6 +2,9 @@ rm -rf ~/.config/nvim
 git clone --depth 1 https://github.com/AstroNvim/template ~/.config/nvim
 rm -rf ~/.config/nvim/.git
 
+mkdir ~/Programming
+git clone https://github.com/GwynnN7/Cortana ~/Programming
+
 libinput-gestures-setup autostart start
 headsetcontrol -s 0
 fc-cache -fv
