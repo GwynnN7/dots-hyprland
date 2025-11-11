d609d8b3f56d3c632b7f2dae58dc86bf7ae3dac7
GwynnN7 <mattcheru03@gmail.com>
2025-11-11 14:10:30 +0100

    Update

--- sdata/custom/scripts.sh
+++ sdata/custom/scripts.sh
@@ -5,6 +5,9 @@ rm -rf ~/.config/nvim/.git
 mkdir -p ~/Programming/Cortana
 git clone https://github.com/GwynnN7/Cortana ~/Programming/Cortana
 
+mkdir -p ~/Programming/dots-hyprland
+git clone https://github.com/GwynnN7/Cortana ~/Programming/dots-hyprland
+
 #headsetcontrol -s 0
 sudo systemctl enable sddm
 fc-cache -f
